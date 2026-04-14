06c6b817c4c6113f550bea871a2c388ecba9fc2d
DoloremIpsumIV <Rjildholt@gmail.com>
2026-04-14 14:18:41 +0200

    Rune SDK implemented

---
+++ GraveFallGame/src/system/Main.js
@@ -0,0 +1,42 @@
+//------------------------------------------------------------------------------
+// Constructor scope
+//------------------------------------------------------------------------------
+
+/**
+ * Creates a new instance of the Main class.
+ *
+ * @constructor
+ * 
+ * @class
+ * @classdesc
+ * 
+ * Entry point class.
+ */
+GraveFallGame.system.Main = function() {
+
+    //--------------------------------------------------------------------------
+    // Super call
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Extend (Rune) Application.
+     */
+    rune.system.Application.call(this, {
+        developer: "se.lnu",
+        app: "GraveFallGame",
+        build: "1.0.0",
+        scene: GraveFallGame.scene.Game,
+        resources: GraveFallGame.data.Requests,
+        useGamepads:true,
+        useKeyboard:true,
+        framerate: 30,
+        debug: true
+    });
+};
+
+//------------------------------------------------------------------------------
+// Inheritance
+//------------------------------------------------------------------------------
+
+GraveFallGame.system.Main.prototype = Object.create(rune.system.Application.prototype);
+GraveFallGame.system.Main.prototype.constructor = GraveFallGame.system.Main;
