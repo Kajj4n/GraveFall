3f0fd2eba741fd89a214b8dd5794f3b8024f5b37
Kajj4n <143015848+Kajj4n@users.noreply.github.com>
2026-05-06 15:18:20 +0200

    can be executable file now

--- GraveFallGame/bin/bootstrap.js
+++ GraveFallGame/bin/bootstrap.js
@@ -1,7 +1,8 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow () {
-    let win = new BrowserWindow({
+    const win = new BrowserWindow({
         width: 1280,
         height: 720,
         useContentSize: true,
@@ -12,7 +13,7 @@ function createWindow () {
     });
 
     win.setBackgroundColor("#000000");
-    win.loadFile('./index.html');
+    win.loadFile(path.join(__dirname, 'index.html'));
     win.setFullScreen(true);
 }
 
